3b956cc59f71a59be7943afbde8e8c7f78b6292a
Carmine Massei <carminemassei@macbook-pro-di-carmine.local>
2018-03-28 23:28:40 +0200

    Initial Commit

---
+++ Plummet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Plummet
+//
+//  Created by Carmine Massei on 28/03/18.
+//  Copyright © 2018 iOSFoundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
